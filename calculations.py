27d463255e0c1cb7817eaf2f5df0387c076d844a
alorthius <anna-alina.bondarets@ucu.edu.ua>
2021-09-28 16:24:16 +0300

    Pylint check

--- calculations.py
+++ calculations.py
@@ -1,28 +1,32 @@
+"""Module to test the algorithms."""
+
+import random
+import time
+import json
+
+# import modules with algorithms implementations
 from selection_sort import selection_sort
 from insertion_sort import insertion_sort
 from merge_sort import merge_sort
 from shellsort import shellsort
-import random
-import logging
-import time
-import json
-from pprint import pprint
 
 
-def generate_random_array(length: int, ascending=False, descending=False, values_list: list = None) -> list:
-    if values_list is None:
-        values_list = [num for num in range(- (length // 2), length // 2 + 1)]
+def generate_random_array(length: int, ascend=False, descend=False, subset: list = None) -> list:
+    """Return randomly generated list with possible options."""
+    if subset is None:
+        subset = [num for num in range(- (length // 2), length // 2 + 1)]
 
-    generated_list = [random.choice(values_list) for _ in range(length)]
+    generated_list = [random.choice(subset) for _ in range(length)]
 
-    if ascending:
+    if ascend:
         return sorted(generated_list)
-    elif descending:
+    elif descend:
         return sorted(generated_list, reverse=True)
     return generated_list
 
 
-def test(task: str, algorithms: list, num_repetitions=5, ascending=False, descending=False, values_list: list = None):
+def test_one_task(task: str, algorithms: list, repetitiions=5, ascend=False, descend=False, subset: list = None) -> dict:
+    """Test all the algorithms and save the results into json files."""
     results_dict = {algorithm.__name__: {} for algorithm in algorithms}
 
     for length_pow in range(7, 16):
@@ -32,23 +36,26 @@ def test(task: str, algorithms: list, num_repetitions=5, ascending=False, descen
             average_time = 0
             average_comparisons = 0
 
-            for repetition in range(1, num_repetitions + 1):
-                array = generate_random_array(array_size, ascending=ascending, descending=descending, values_list=values_list)
+            for _ in range(1, repetitiions + 1):
+                array = generate_random_array(
+                    array_size, ascend=ascend, descend=descend, subset=subset)
 
+                # compute time taken and comparisons number
                 start = time.time()
-                new_array, comparisons = algorithm(array)
+                _, comparisons = algorithm(array)
                 end = time.time()
 
                 average_time += end - start
                 average_comparisons += comparisons
 
-            average_time = average_time / 5
-            average_comparisons = average_comparisons / 5
+            # find average time taken and comparisons number
+            average_time = average_time / repetitiions
+            average_comparisons = average_comparisons / repetitiions
 
             results_dict[algorithm.__name__][length_pow] = [
                 average_time, average_comparisons]
 
-    pprint(results_dict)
+    # save the results
     with open(f'{task}.json', 'w') as json_file:
         json.dump(results_dict, json_file)
 
@@ -56,9 +63,9 @@ def test(task: str, algorithms: list, num_repetitions=5, ascending=False, descen
 
 
 if __name__ == '__main__':
-    algorithms = [selection_sort, insertion_sort, merge_sort, shellsort]
+    algorithms_list = [selection_sort, insertion_sort, merge_sort, shellsort]
 
-    test('task_1', algorithms)
-    test('task_2', algorithms, num_repetitions=1, ascending=True)
-    test('task_3', algorithms, num_repetitions=1, descending=True)
-    test('task_4', algorithms, num_repetitions=3, values_list=[1, 2, 3])
+    test_one_task('task_1', algorithms_list)
+    test_one_task('task_2', algorithms_list, repetitiions=1, ascend=True)
+    test_one_task('task_3', algorithms_list, repetitiions=1, descend=True)
+    test_one_task('task_4', algorithms_list, repetitiions=3, subset=[1, 2, 3])
